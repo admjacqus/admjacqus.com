bf49fa9c392e5a9bf7514b8b9e706a15870af29f
Adam Jacques <adam.jacques@missguided.com>
2019-02-02 12:05:26 +0000

    :boom: 2019 initial commit

---
+++ _main.js
@@ -0,0 +1,6 @@
+(function(jQuery) {
+    // we can now rely on jQuery within the safety of our "bodyguard" function
+    jQuery(document).ready(function() {
+      console.log('waddup');
+    });
+  })(jQuery);
