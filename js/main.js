36fe055feda5f5bdeecf8f7bab05d5449cc753fc
Adam Jacques <adam.jacques@missguided.com>
2019-02-03 17:26:16 +0000

    meh

--- js/main.js
+++ js/main.js
@@ -11,18 +11,41 @@ $('nav.big-menu li a').hover(
     }
 );
 
-$('section.sites ul li a:not(.link)').hover(
-    function () {
-        //we get our current filename and use it for the src
-        var linkIndex = $(this).attr("data-filename");
-        $('section.sites ul li a:not(.link)').removeClass('active');
-        $(this).addClass('active');
-        $('#site-preview').attr('src', 'images/sites/' + linkIndex + '.jpeg');
-        $('#modal').removeClass("site1 site2 site3 site4 site5 site6").addClass(linkIndex);
+$(function() {
+    $('body').addClass('ready');
+    // console.log( "ready!" );
 
-        // $('#modal').attr('class', linkIndex);
-    }
-);
+});
+
+var myArray = [
+    "https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549208806/11-12/Designs_Lookbook_2012-14.jpg",
+    "https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549208803/11-12/Designs_Lookbook_2012-20.jpg",
+    "https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549208796/11-12/Designs_Lookbook_2012-13.jpg",
+    "https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549208790/11-12/Designs_Lookbook_2012-16.jpg",
+    "https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549209421/11-12/colummash-pinkrobot.png",
+    "https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549208769/11-12/Designs_Lookbook_2012-11.jpg","https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549208743/11-12/Designs_Lookbook_2012-02.jpg","https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549208753/11-12/Designs_Lookbook_2012-09.jpg","https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549208793/11-12/Designs_Lookbook_2012-10.jpg","https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549209298/11-12/eddiesTORM.png","https://res.cloudinary.com/dsvt6q8vh/image/upload/v1549209413/11-12/colummash-y-01.png"
+  ];
+  
+
+$('img.random').click(function(){
+  var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+
+    $('img.random').attr('src', randomItem );
+    return false;
+});
+
+// $('section.sites ul li a:not(.link)').hover(
+//     function () {
+//         //we get our current filename and use it for the src
+//         var linkIndex = $(this).attr("data-filename");
+//         $('section.sites ul li a:not(.link)').removeClass('active');
+//         $(this).addClass('active');
+//         $('#site-preview').attr('src', 'images/sites/' + linkIndex + '.jpeg');
+//         $('#modal').removeClass("site1 site2 site3 site4 site5 site6").addClass(linkIndex);
+
+//         // $('#modal').attr('class', linkIndex);
+//     }
+// );
 
 $.cloudinary.responsive();
 
@@ -37,10 +60,6 @@ $.cloudinary.responsive();
 //     ];
 //     document.getElementById("random").style.backgroundImage = bigSize[random];
 // }
-
-$(document).ready(function () {
-    $('#DVD').DVDBounce();
-});
 
 $(".hi").click(function () {
     // alert("Handler for .click() called.");
